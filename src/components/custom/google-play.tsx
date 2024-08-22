7ed58399a008630d5d10cacc5f4d6b36b1c00d38
quingsley <jeromejuma25@gmail.com>
2024-08-22 19:22:29 +0300

    refactor: Update Google Play button link to use Play Store URL

--- src/components/custom/google-play.tsx
+++ src/components/custom/google-play.tsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function GooglePlayButton() {
   return (
-    <Link to="https://appdistribution.firebase.google.com/testerapps/1:581400212875:android:a68d8eeea6323ec5a7ab8e/releases/6l3s3l6k8inpg?utm_source=firebase-console">
+    <Link to="https://play.google.com/store/apps/details?id=com.quingsley.carbon_zero">
       <div className="flex flex-row mt-10 px-4 py-2 rounded-xl border-2 justify-center items-center border-primary w-56 cursor-pointer hover:bg-primary-foreground">
         <div>
           <img src="/google-play.png" alt="google play icon" height={60} width={60} />
